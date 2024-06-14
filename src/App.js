efdacc0fa32a15ca1b9fc53d3d0f34d711e31ada
Sri Raja Vignesh <srirajarox@gmail.com>
2024-06-14 22:27:35 +0530

    added side nav bar and added a gitignore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import SideNavBar from "./components/SideNavBar/SideNavBar";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const tabNames = ["About Me", "Experience", "Projects", "Skills", "Contact"];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SideNavBar
+        tabs={tabNames}
+        selectedTab={selectedTab}
+        setSelectedTab={setSelectedTab}
+      />
     </div>
   );
 }
---
+++ src/components/SideNavBar/SideNavBar.js
@@ -0,0 +1,27 @@
+import styles from "./SideNavBar.module.css";
+
+const SideNavBar = ({ tabs, selectedTab, setSelectedTab }) => {
+  const selectedTabStyle = {
+    color: "black",
+    fontSize: "25px",
+    fontWeight: 600,
+  };
+
+  return (
+    <div className={styles.nav_body}>
+      {tabs.map((tab, index) => {
+        return (
+          <div
+            className={styles.nav_element}
+            style={selectedTab === index ? selectedTabStyle : null}
+            onClick={() => setSelectedTab(index)}
+          >
+            {tab}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default SideNavBar;
